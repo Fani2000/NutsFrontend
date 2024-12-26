982f5162238a59b64607c4e763bc33881eca8c4c
Keorapetse Fani <fkeorapetse@quickloc8.co.za>
2024-12-26 03:11:58 +0200

    updated the homeview to cleanup

--- Nutsshop/src/View/HomeView.vue
+++ Nutsshop/src/View/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import Header from '../Components/Header.vue'
 // Example sample data for nuts products
 const products = [
   {
@@ -28,34 +29,7 @@ const products = [
 <template>
   <div>
     <!-- Heading -->
-    <div class="px-6 h-[330px] mb-6 bg-[url('./assets/images/background.svg')]">
-      <div class="relative top-10 flex flex-col gap-20 text-white bg-gradient-to-b from-transparent">
-        <div class="flex justify-between">
-          <div>
-            <img src="../assets/Images/logo.svg" />
-          </div>
-          <div class="flex gap-6">
-            <div>
-              <img src="../assets/Images/search.svg" />
-            </div>
-            <div>
-              <img src="../assets/Images/cart.svg" />
-            </div>
-          </div>
-        </div>
-        <div class="">
-          <h1 class="text-4xl font-bold">Discover Nature's Bounty</h1>
-          <p class="text-xl mt-2">
-            Discover our premium selection of fresh and healthy nuts just for you!
-          </p>
-          <p class="mt-2">
-            Fuel your body and mind with nature's most delicious energy source.
-            Explore our wide variety of nuts and discover the perfect fit for your
-            healthy lifestyle.
-          </p>
-        </div>
-      </div>
-    </div>
+    <Header />
 
     <!-- Product Grid -->
     <div class="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
