8aa1a63f138242cad681ebf6a00b1d0b5a29ae95
Keorapetse Fani <fkeorapetse@quickloc8.co.za>
2024-12-31 12:19:55 +0200

    Updated the Home view

--- Nutsshop/src/View/HomeView.vue
+++ Nutsshop/src/View/HomeView.vue
@@ -40,7 +40,7 @@ const products = [
     <Header />
 
     <!-- Product Grid -->
-    <div class="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div class="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 pb-10">
       <!-- Product Card -->
       <ProductCart :products="[...products, ...products, ...products]" />
     </div>
